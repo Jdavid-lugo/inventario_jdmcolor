3c0ead759c402a68581ca3f205927ed6c12aee42
Jose David Lugo <jose.lugo@dinmax.com>
2021-04-05 13:08:18 -0400

    translating language

--- resources/views/sales/addproduct.blade.php
+++ resources/views/sales/addproduct.blade.php
@@ -7,7 +7,7 @@
                     <div class="card-header">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">Add Product</h3>
+                                <h3 class="mb-0">Agregar producto</h3>
                             </div>
                             <div class="col-4 text-right">
                                 <a href="{{ route('sales.show', [$sale->id]) }}" class="btn btn-sm btn-primary">Volver a la lista</a>
